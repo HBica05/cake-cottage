234c2091535b6d80497639264564d682cd8ca787
HBica05 <bicahaadiyah@gmail.com>
2025-02-06 14:46:05 +0200

    create django template to render recipes and comments

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe, Comment
+from .forms import RecipeForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    comments = recipe.comments.all()
+    if request.method == "POST":
+        content = request.POST.get('content')
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.recipe = recipe
+            new_comment.user = request.user
+            new_comment.save()
+            return redirect('recipe_detail', pk=pk)
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe, 'comments': comments, 'comment_form': comment_form})
