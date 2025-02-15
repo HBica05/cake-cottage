a2db85b485dcc24288b9126508b62af83c60181b
HBica05 <bicahaadiyah@gmail.com>
2025-02-15 12:36:30 +0200

    Add Featured Products section in homepage

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,3 @@
-# urls.py
 from django.urls import path
 from . import views
 
@@ -7,5 +6,14 @@ urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
     
     # URL for displaying a specific recipe and its comments
-    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+
+     # URL for adding a comment to a recipe
+    path('comment/add/<int:recipe_id>/', views.add_comment, name='add_comment'),
+    
+    # URL for editing a comment
+    path('comment/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    
+    # URL for deleting a comment
+    path('comment/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
