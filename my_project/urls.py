2651dc5843cfb8370b76db967241f398e96af2f7
HBica05 <bicahaadiyah@gmail.com>
2025-02-27 15:26:07 +0200

    Fixed include import issue in urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,23 +1,21 @@
-"""
-URL configuration for my_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # ✅ Ensure 'include' is imported
+
+from . import views
+from .views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
+    path('', views.recipe_list, name='recipe_list'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('recipe/create/', views.recipe_create, name='recipe_create'),
+
+    path('recipes/', include('recipes.urls')),  # ✅ Ensure this line is correct
+
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path('comment/add/<int:recipe_id>/', views.add_comment, name='add_comment'),
+    path('comment/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path('comment/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path("register/", register, name="register"),
 ]
