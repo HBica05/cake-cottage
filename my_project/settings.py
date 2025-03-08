8f2f2fd3b1017906eb20afc2a31565b2178df151
HBica05 <bicahaadiyah@gmail.com>
2025-03-08 15:27:42 +0200

    Deploy latest changes to Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
