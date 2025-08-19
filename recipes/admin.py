efe888a89762a546816a02416ad8ef46a84a8e0c
HBica05 <bicahaadiyah@gmail.com>
2025-08-19 14:01:26 +0200

    Customize Django admin for recipes, comments, and likes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Recipe, Comment, Like
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "status", "category", "created_at")
@@ -8,22 +9,40 @@ class RecipeAdmin(admin.ModelAdmin):
     search_fields = ("title", "description", "ingredients", "instructions", "author__username")
     prepopulated_fields = {"slug": ("title",)}
     ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+    raw_id_fields = ("author",)
+
+
+@admin.action(description="Approve selected comments")
+def approve_comments(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+
+
+@admin.action(description="Unapprove selected comments")
+def unapprove_comments(modeladmin, request, queryset):
+    queryset.update(is_approved=False)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("id", "recipe", "user", "body", "created_at")
+    list_display = ("short_body", "user", "recipe", "is_approved", "created_at")
     list_filter = ("is_approved", "created_at", "recipe")
     search_fields = ("body", "user__username", "recipe__title")
     ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+    raw_id_fields = ("user", "recipe")
+    actions = (approve_comments, unapprove_comments)
 
     def short_body(self, obj):
         txt = obj.body or ""
-        return (txt[:50] + "…") if len(txt) > 50 else txt
-    short_body.short_description = "body"
+        return (txt[:60] + "…") if len(txt) > 60 else txt
+    short_body.short_description = "Comment"
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ("recipe", "user")
+    list_display = ("user", "recipe", "created_at")
+    list_filter = ("created_at",)
     search_fields = ("recipe__title", "user__username")
+    raw_id_fields = ("user", "recipe")
+    date_hierarchy = "created_at"
